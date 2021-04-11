fcd8d1fde35ff864bca43b4aede35151bae1c198
maria <maria.m.portela@gmail.com>
2021-04-11 19:36:17 +0100

    Add input parameters for primer length and GC content

--- primersUI/app.py
+++ primersUI/app.py
@@ -14,25 +14,38 @@ app.config.from_mapping(
 # start page where the user inputs sequence and preferences
 @app.route('/', methods=['GET', 'POST'])
 def start():
+    error = None
     if request.method == 'POST':
         exon1 = request.form['exon1']
         exon2 = request.form['exon2']
-        error = None
+        gc_content = request.form['gc-content']
+        primer_length = request.form['primer-length']
 
         # form validation
-        if not exon1:
-            error = 'Exon 1 sequence is required.'
-        elif not exon2:
-            error = 'Exon 2 sequence is required.'
+        try:
+            int(gc_content)
+        except:
+            error = "Please insert valid number for GC content"
 
+        try:
+            int(primer_length)
+        except:
+            error = "Please insert valid number for primer length"
+
+        if len(exon1) < len(primer_length) or len(exon2) < len(primer_length):
+            error = "The number of nucleotides must be bigger than primer length"
+
+        # No errors, process input and show results page
         if error is None:
-            hits = script.hits_from_exons(exon1, exon2)
+            hits = script.hits_from_exons(exon1, exon2, int(gc_content), int(primer_length))
             session["hits"] = hits
             return redirect(url_for('results'))
 
+        # Form has errors
         flash(error)
 
     return render_template('start.html')
+
 
 # show results page with primer candidates
 @app.route('/results')
--- primersUI/script.py
+++ primersUI/script.py
@@ -1,37 +1,40 @@
 # Basic script to find primer candidates
 # Hits are 20bp in length, with 50-55% GC content, GC clamps in the 3' end, and no more than 3xGC at the clamp
+from math import (floor, ceil)
+
 
 # Function that receives a gene string (only C, G, T or A characters) and outputs an array of primer hits
 # Looks for a GC clamp reading from start to end of the string
-def find_hit_in_sequence(gene):
+def find_hit_in_sequence(gene, gc_target, length):
     hits = []
-    for i in range(len(gene) - 20):
+    for i in range(len(gene) - length):
         if gene[i] == 'C' or gene[i] == 'G':
             if gene[i + 1] == 'C' or gene[i + 1] == 'G':
                 if gene[i + 2] != 'C' and gene[i + 2] != 'G':
-                    cg_count = 0
-                    for base in gene[i:i + 20]:
+                    gc_count = 0
+                    for base in gene[i:i + length]:
                         if base == 'C' or base == 'G':
-                            cg_count += 1
+                            gc_count += 1
 
-                    if cg_count == 10 or cg_count == 11:
-                        hits.append(gene[i:i + 20])
+                    # approximate the requested GC%
+                    if gc_count == ceil(gc_target*length/100) or gc_count == ceil(gc_target*length/100)-1:
+                        hits.append(gene[i:i + length])
     return hits
 
 
-def hits_from_exons(exon1, exon2):
+def hits_from_exons(exon1, exon2, gc_content, primer_length):
     # Remove white spaces or irrelevant characters
-    exon1 = exon1.strip()
-    exon2 = exon2.strip()
+    exon1 = "".join(exon1.split())
+    exon2 = "".join(exon1.split())
 
     # Reverse exon 1 as GC clamp should be at the end of the primer
     exon1 = exon1[::-1]
     hits_exon1 = []
-    for elem in find_hit_in_sequence(exon1):
+    for elem in find_hit_in_sequence(exon1, gc_content, primer_length):
         hits_exon1.append(elem[::-1])
 
     # Find hits for exon 2
-    hits_exon2 = find_hit_in_sequence(exon2)
+    hits_exon2 = find_hit_in_sequence(exon2, gc_content, primer_length)
 
     return {
         "forward": hits_exon1,
